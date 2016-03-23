c82f42468f3bdf5cdc534ec2255c6f4fc3527c32
Rob Shakir <rjs@jive.com>
2016-03-23 02:01:28 -0600

    Add generated code - include README, push to GitHub.

---
+++ grpc/server/grpc_srv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "common"))
+
+import pynms_rpc_pb2
+
+
+class NetElem(pynms_rpc_pb2.BetaOCPyNMSServicer):
+  def Get(self, request, context):
+    return pynms_rpc_pb2.GetResponse(path=["some", "path"], type=pynms_rpc_pb2.IETF_JSON, value="{'test': null}")
+
+def serve():
+  server = pynms_rpc_pb2.beta_create_OCPyNMS_server(NetElem())
+  server.add_insecure_port('[::]:50051')
+  server.start()
+  try:
+    while True:
+      time.sleep(84600)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+if __name__ == '__main__':
+  serve()
